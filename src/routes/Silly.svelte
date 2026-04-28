9c55c3195eb582e6d04462ea44d0c7a6687568a4
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-04-28 22:03:04 +0300

    fix: always transition Silly
    fixes an issue where Silly would jump up without a transition if you
    resize the browser window to be small enough.

--- src/routes/Silly.svelte
+++ src/routes/Silly.svelte
@@ -4,6 +4,6 @@
 
 <img
 	src={image}
-	class="fixed right-0 bottom-0 w-32 animate-slide-in xl:right-4 xl:translate-y-1/3 xl:transition-transform xl:hover:translate-y-0"
+	class="fixed right-0 bottom-0 w-32 animate-slide-in transition-transform xl:right-4 xl:translate-y-1/3 xl:hover:translate-y-0"
 	alt="custom heart emoji"
 />
