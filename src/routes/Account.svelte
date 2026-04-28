51aedb700757ffafe9ba8b7342d6e09da6122500
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-04-28 09:38:49 +0300

    feat: add hover animation to Accounts

--- src/routes/Account.svelte
+++ src/routes/Account.svelte
@@ -4,4 +4,9 @@
 	const { children, href }: { children: Snippet; href: string } = $props();
 </script>
 
-<a {href} rel="external" class="h-8 w-8 text-purple-300 *:h-full *:w-full">{@render children()}</a>
+<a
+	{href}
+	rel="external"
+	class="h-8 w-8 text-purple-300 transition-all *:h-full *:w-full hover:brightness-110"
+	>{@render children()}</a
+>
