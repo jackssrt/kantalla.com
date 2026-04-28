ed9749fe7cdf4af20b20df57df58c032e4a1c509
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-04-28 14:42:18 +0300

    feat: add fade in animation

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -4,8 +4,11 @@
 	const { children }: { children: Snippet } = $props();
 </script>
 
-<div
-	class="flex h-fit w-fit flex-col items-center gap-2 rounded border border-purple-300 bg-purple-400/50 p-2 px-4 shadow backdrop-blur-2xl transition-all hover:bg-purple-400/55"
->
-	{@render children()}
+<div class="relative h-fit w-fit overflow-clip rounded">
+	<div class="absolute inset-0 -z-10 animate-fade-in-backdrop"></div>
+	<div
+		class="flex h-full w-full animate-fade-in flex-col items-center gap-2 overflow-clip rounded border border-purple-300 bg-purple-400/50 p-2 px-4 shadow transition-all hover:bg-purple-400/55"
+	>
+		{@render children()}
+	</div>
 </div>
--- src/routes/ProfilePicture.svelte
+++ src/routes/ProfilePicture.svelte
@@ -3,7 +3,7 @@
 	import Heart from '$lib/assets/heart.svg?component';
 </script>
 
-<div class="relative">
+<div class="relative animate-fade-in">
 	<Heart class="absolute bottom-0 left-0 h-auto w-16" />
 	<img src={avatar} alt="Profile" class="w-48 rounded-full border-2 border-purple-300" />
 	<Heart class="absolute top-0 right-0 -z-10 h-auto w-16" />
