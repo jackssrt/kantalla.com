d739f3c33719751862bd03d7f862358b94ab1428
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-04-28 22:00:47 +0300

    fix: update Credits

--- src/routes/Credits.svelte
+++ src/routes/Credits.svelte
@@ -2,8 +2,16 @@
 	import ExternalLink from '$lib/components/ExternalLink.svelte';
 </script>
 
-<p class="max-w-48 text-white/50 xl:fixed xl:bottom-2 xl:left-2 xl:max-w-md">
-	<ExternalLink href="https://github.com/jackssrt/kantalla.com"
-		>Licensed under the GPLv3</ExternalLink
-	>, made with love by <ExternalLink href="https://jackssrt.com">jackssrt</ExternalLink> :3
+<p
+	class="group max-w-48 text-sm text-white/50 transition-colors hover:text-white xl:fixed xl:bottom-2 xl:left-2 xl:max-w-md"
+>
+	<ExternalLink href="https://github.com/jackssrt/kantalla.com">Open source</ExternalLink>, <ExternalLink
+		href="https://github.com/jackssrt/kantalla.com/LICENSE">copyleft GPLv3</ExternalLink
+	>, made with <span class="group-hover:text-purple-400">love</span> by <ExternalLink
+		href="https://jackssrt.com">jackssrt</ExternalLink
+	> :3<br />
+	The Quaver logo is licensed under <ExternalLink
+		href="https://creativecommons.org/licenses/by-nc/4.0/legalcode">CC BY-NC 4.0</ExternalLink
+	><br />
+	Other logos are property of their respective trademark holders
 </p>
