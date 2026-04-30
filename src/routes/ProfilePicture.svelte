3eafe043409e622c210b59aedcb84f3deb8dc59f
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-04-30 14:05:57 +0300

    chore: prevent user from selecting ProfilePicture

--- src/routes/ProfilePicture.svelte
+++ src/routes/ProfilePicture.svelte
@@ -5,6 +5,10 @@
 
 <div class="relative animate-fade-in">
 	<Heart class="absolute bottom-0 left-0 h-auto w-16" />
-	<img src={avatar} alt="Profile" class="w-48 rounded-full border-2 border-purple-300 shadow-xl" />
+	<img
+		src={avatar}
+		alt="Profile"
+		class="w-48 rounded-full border-2 border-purple-300 shadow-xl select-none"
+	/>
 	<Heart class="absolute top-0 right-0 -z-10 h-auto w-16" />
 </div>
