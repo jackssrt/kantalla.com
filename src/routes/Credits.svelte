bf541f15809893b5135c2a85bd1164fe25292c9e
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-05-09 23:29:56 +0300

    feat: hide third party licenses in a details tag

--- src/routes/Credits.svelte
+++ src/routes/Credits.svelte
@@ -2,18 +2,24 @@
 	import ExternalLink from '$lib/components/ExternalLink.svelte';
 </script>
 
-<p
-	class="group max-w-48 text-sm text-white/50 transition-colors hover:text-white xl:fixed xl:bottom-2 xl:left-2 xl:max-w-md"
+<div
+	class="max-w-48 text-sm text-white/50 transition-colors hover:text-white xl:fixed xl:bottom-2 xl:left-2 xl:max-w-md"
 >
-	<ExternalLink href="https://github.com/jackssrt/kantalla.com">Open source</ExternalLink>, <ExternalLink
-		href="https://github.com/jackssrt/kantalla.com/LICENSE">copyleft GPLv3</ExternalLink
-	>, made with <span class="group-hover:text-purple-400">love</span> by <ExternalLink
-		href="https://jackssrt.com">jackssrt</ExternalLink
-	> :3<br />
-	The Quaver logo is licensed under <ExternalLink
-		href="https://creativecommons.org/licenses/by-nc/4.0/legalcode">CC BY-NC 4.0</ExternalLink
-	><br />
-	Other logos are property of their respective trademark holders <br />Sound effects by <ExternalLink
-		href="https://pixabay.com/users/floraphonic-38928062/">floraphonic</ExternalLink
-	> from <ExternalLink href="https://pixabay.com//">Pixabay</ExternalLink>
-</p>
+	<p class="group">
+		<ExternalLink href="https://github.com/jackssrt/kantalla.com">Open source</ExternalLink>, <ExternalLink
+			href="https://github.com/jackssrt/kantalla.com/LICENSE">copyleft GPLv3</ExternalLink
+		>, made with <span class="group-hover:text-purple-400">love</span> by <ExternalLink
+			href="https://jackssrt.com">jackssrt</ExternalLink
+		> :3<br />
+	</p>
+
+	<details>
+		<summary class="select-none"> Third party licenses </summary>
+		The Quaver logo is licensed under <ExternalLink
+			href="https://creativecommons.org/licenses/by-nc/4.0/legalcode">CC BY-NC 4.0</ExternalLink
+		><br />
+		Other logos are property of their respective trademark holders <br />Sound effects by <ExternalLink
+			href="https://pixabay.com/users/floraphonic-38928062/">floraphonic</ExternalLink
+		> from <ExternalLink href="https://pixabay.com//">Pixabay</ExternalLink>
+	</details>
+</div>
